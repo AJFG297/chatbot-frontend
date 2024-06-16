595c20fb51bf73744708c58d1e6decd95ce6e116
AJFG297 <502145@student.fontys.nl>
2024-06-16 18:25:58 +0200

    Update GptApi.js

--- chatbot/src/apis/GptApi.js
+++ chatbot/src/apis/GptApi.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_KEY = "sk-proj-KnphZJpjIZ2mZ765mqf5T3BlbkFJ9n8tASCBjpOQHsabv9si"; // Your API key here
-console.log(API_KEY);
+// const API_KEY = ""; // Your API key here
+// console.log(API_KEY);
 
-export const getAIResponse = async (message) => {
-    const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-            model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: message }],
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${API_KEY}`,
-            },
-        }
-    );
-    return response.data.choices[0].message.content;
-};
+// export const getAIResponse = async (message) => {
+//     const response = await axios.post(
+//         'https://api.openai.com/v1/chat/completions',
+//         {
+//             model: 'gpt-3.5-turbo',
+//             messages: [{ role: 'user', content: message }],
+//         },
+//         {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': `Bearer ${API_KEY}`,
+//             },
+//         }
+//     );
+//     return response.data.choices[0].message.content;
+// };
